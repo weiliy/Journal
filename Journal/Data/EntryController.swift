df47e41ef44ce08490859bba2f7bd0bd2fcb7cf8
Weili Yi <wlyi@thoughtworks.com>
2018-09-20 10:24:42 +0800

    WORKING WITH UITABLEVIEW

--- Journal/Data/EntryController.swift
+++ Journal/Data/EntryController.swift
@@ -14,7 +14,21 @@ class EntryController {
     
     var entries: [Entry] = []
     var tags: [String] {
-        return []
+        var tags = Set<String>()
+        entries.forEach {
+            tags.update(with: $0.tag.capitalized)
+        }
+        return Array(tags).sorted()
+    }
+    
+    init() {
+        let entry1 = Entry(title: "My first Entry", body: "this is y first entry", tag: "Journal", color: .lightGray)
+        let entry2 = Entry(title: "My 2 Entry", body: "this is y first entry", tag: "Journal", color: .lightGray)
+        let entry3 = Entry(title: "My 3 Entry", body: "this is y first entry this is y first entrythis is y first entrythis is y first entrythis is y first entrythis is y first entrythis is y first entrythis is y first entrythis is y first entrythis is y first entry", tag: "Journal", color: .lightGray)
+        let entry4 = Entry(title: "My 4 Entry", body: "this is y first entry", tag: "journal", color: .lightGray)
+        let entry5 = Entry(title: "My 5 Entry", body: "this is y first entry", tag: "Journal", color: .lightGray)
+        
+        entries = [entry1, entry2, entry3, entry4, entry5]
     }
     
     // CRUD
@@ -29,13 +43,13 @@ class EntryController {
     }
     
     func getEntry(at indexPath: IndexPath) -> Entry {
-        let tag = tags[indexPath.section]
-        let entries = getEntries(with: tag)
+        // let tag = tags[indexPath.section]
+        // let entries = getEntries(with: tag)
         return entries[indexPath.row]
     }
     
     func getEntries(with tag: String) -> [Entry] {
-        return entries.filter { $0.tag == tag }
+        return entries.filter { $0.tag.lowercased() == tag.lowercased() }
     }
     
     func updateEntry(_ entry: Entry,
